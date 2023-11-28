8b3340deec5d7260806924cc5b38c31775a11db2
Josh Larson <113719434+JoshyLar@users.noreply.github.com>
2023-11-27 19:06:23 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Vector;
@@ -30,81 +31,11 @@ import java.lang.Runnable;
 
 
 public class Main {
-    /*public static String[] teamNames = new String[]{"White Sox", "Cubs", "Guardians", "Reds", "Tigers", "Brewers", "Royals", "Pirates", "Twins", "Cardinals", "Orioles", "Braves", "Red Sox", "Marlins", "Yankees", "Mets", "Rays", "Phillies", "bluejays", "Nationals", "Astros", "dbacks", "Angels", "Rockies", "Athletics", "Dodgers", "Mariners", "Padres", "rangers", "Giants"};
-
     public static void main(String[] args) {
-        for(int i = 0 ; i < teamNames.length;i++) {
-            teamNames[i] = teamNames[i].toLowerCase().replaceAll(" ", "");
-        }
-
-        try{
-            // Loading (fetching and parsing the HTML)
-            for(String team: teamNames){
-            String urlName = String.format("https://www.mlb.com/%s/roster", team);
-            Document doc = Jsoup.connect(urlName).get();
-            Elements link = doc.select("td.info > a") ;
-
-            //Elements values = doc.select("td") ;
-            //for(Element el:link) {
-                System.out.println(link.attr("href"));
-            //}
-                System.out.println(team);
-            }
-
-
-
-
-
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
-
-      /* try{
-        // Loading (fetching and parsing the HTML)
-        String urlName = "https://www.mlb.com/standings";
-        Document doc = Jsoup.connect(urlName).get();
-        //Elements link = doc.select("tdtext") ;
-
-        Elements values = doc.select("td.text") ;
-        System.out.println(values);
-
-
-
-
-
-
-    }catch (Exception e) {
-        e.printStackTrace();
-    }
-}*/
-    //public static Vector<String> teamNames = new Vector<>() ;
-
-    public static String[] teamNames = new String[]{"White Sox", "Cubs", "Guardians", "Reds", "Tigers", "Brewers", "Royals", "Pirates", "Twins", "Cardinals", "Orioles", "Braves", "Red Sox", "Marlins", "Yankees", "Mets", "Rays", "Phillies", "bluejays", "Nationals", "Astros", "dbacks", "Angels", "Rockies", "Athletics", "Dodgers", "Mariners", "Padres", "rangers", "Giants"};
-    public static MLB_DB_Object tester = new MLB_DB_Object("jdbc:sqlite:/Users/joshualarson/MLB_Q_STAT.db") ;
-
-    public static void main(String[] args) {
-//        Date start = new Date();
-//       MLB_Scraper2 scrapee = new MLB_Scraper2();
-
-        MlbScraper2 scrapah = new MlbScraper2();
-       // scrapah.selina();
-//
-//       // Vector <Vector<String>> rosters = new Vector<>() ;
-//        List<List<String>> rosters2 = new Vector<>() ;
-//       // rosters = scrapee.fetch_rosters() ;
-//        //scrapee.fetch_player_stats(rosters);
-//       rosters2 = scrapah.fetchRostersConcurrently() ;
-//        //scrapah.fetch_player_stats(rosters2);
-//        Date end = new Date();
-//        System.out.println(end.getTime()-start.getTime());
- //   MLB_DB_Object tester = new MLB_DB_Object("jdbc:sqlite:/Users/joshualarson/MLB_Q_STAT.db") ;
-    List player  =  scrapah.stat_prep();
-    for(Object i : player)
-        System.out.println(i);
-    }
-
-
+        MlbDatabaseManager dbManager = new MlbDatabaseManager();
+        dbManager.updateStatsP();
+}
 
 
 }
+
