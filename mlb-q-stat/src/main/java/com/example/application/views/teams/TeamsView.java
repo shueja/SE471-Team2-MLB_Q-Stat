9b6f643bfe67f6a458d273462364d860e5bf5d98
shueja-personal <j.a.shue@outlook.com>
2024-04-19 23:07:57 -0700

    amend previous commit

--- mlb-q-stat/src/main/java/com/example/application/views/teams/TeamsView.java
+++ mlb-q-stat/src/main/java/com/example/application/views/teams/TeamsView.java
@@ -2,6 +2,7 @@ package com.example.application.views.teams;
 
 import com.example.application.data.TeamService;
 import com.example.application.data.entity.Team;
+import com.example.application.data.enums.TeamStat;
 import com.example.application.views.MainLayout;
 import com.example.application.views.players.GenericPlayersView;
 import com.vaadin.flow.component.dependency.Uses;
@@ -19,7 +20,7 @@ public class TeamsView extends GenericPlayersView {
     public TeamsView(TeamService teamService) {
         super(
                 Team.class,
-                TeamService.TeamStat.class,
+                TeamStat.class,
                 teamService.getAllTeams(),
                 LitRenderer
                         .<Team>of("<a href=\"/team/${item.acronym}\">${item.name}</a>")
